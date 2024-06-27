873e776be2f6adf8020946d3838f473e339f3a41
Mazylx <dragonmma005@gmail.com>
2024-06-27 10:48:37 +0700

    2 commit

---
+++ Pracktice/Data.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pracktice
+{
+    public class Data
+    {
+        public static PracticeEntities db = new PracticeEntities();
+
+        public static User GetUserByLogin(string login, string password)
+        {
+            return db.User.FirstOrDefault(user => user.Login == login && user.Password == password);
+        }
+
+        public static Role GetUserRole(string roleName)
+        {
+            return db.Role.FirstOrDefault(role => role.Name == roleName);
+        }
+
+        public static bool CheckUserRole(string login, string password, string roleName)
+        {
+            User user = GetUserByLogin(login, password);
+            if (user != null)
+            {
+                Role role = db.Role.FirstOrDefault(r => r.Name == roleName && r.Id == user.RoleID);
+                if (role != null && role.Name == "Admin")
+                {
+                    return true; //если пользователь админ 
+                }
+            }
+            return false; // если его не нашли или  он не админ
+        }
+
+    }
+}
+
---
+++ Pracktice/vhod.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Pracktice
+{
+    /// <summary>
+    /// Логика взаимодействия для vhod.xaml
+    /// </summary>
+    public partial class vhod : Page
+    {
+        public vhod()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            string login = LoginTextBox.Text;
+            string password = PaswordTextBox.Text;
+            Data.GetUserByLogin(login, password);
+            User user = Data.GetUserByLogin(login, password);
+            if (user != null)
+            {
+                NavigationService.Navigate(new Uri("/HomePage.xaml", UriKind.Relative));
+            }
+            else
+            {
+                warning.Content = "неправильный пароль или логин!";
+            }
+        }
+
+        private void LoginTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void PaswordTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
